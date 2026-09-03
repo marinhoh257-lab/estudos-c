d2296927354e2597fd1c9003c3b3f7591ed5d0a8
Henrique Soares Marinho <marinhoh257@gmail.com>
2026-09-03 18:27:19 -0300

    feat: adiciona código de demonstração do tipo float.

---
+++ 01-variaveis/02-float_operacao.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    float preco = 19.99f;
+    int quantidade = 3;
+    float total = preco * quantidade;
+
+    printf("Preço: %.2f\n", preco);
+    printf("Total: %.2f\n", total);
+
+    return 0;
+}
